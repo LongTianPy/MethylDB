1e89303dfbb076e9cf282640c633cd0130b66672
LongTianPy <tianlongapp@gmail.com>
2018-06-05 16:13:02 -0500

    add home

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '23.101.125.241/MethylDB/';
+$config['base_url'] = 'http://23.101.125.241/MethylDB';
 
 /*
 |--------------------------------------------------------------------------
