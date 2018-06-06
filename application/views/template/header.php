e508966206bf2f3022f8db6b9190dfdec17a9483
LongTianPy <tianlongapp@gmail.com>
2018-06-06 12:31:43 -0500

    changed header

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -2,11 +2,15 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-    <div class="" style="">
-        <h1>Pan-cancer DNA methylation pattern mining and visualization for biomarker discovery</h1>
-        <ul class="nav navbar-nav">
-            <li><a href="home" style="">Home</a></li>
-            <li><a href="" style="">About</a></li>
+    <span class="navbar-brand mb-0 h1">MethylDB</span>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="welcome">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="about">About</a>
+            </li>
         </ul>
     </div>
 </nav>
