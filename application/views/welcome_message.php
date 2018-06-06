a83aed5abf13ccfa80bc8dc828872b8ccc858054
LongTianPy <tianlongapp@gmail.com>
2018-06-05 21:32:20 -0500

    manually added bootstrap css and js

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -7,7 +7,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo base_url() . 'CSS/bootstrap.min.css'; ?>" >
 
     <title>Pan-cancer DNA methylation pattern mining and visualization for biomarker discovery</title>
 </head>
@@ -18,8 +18,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <?php $this->load->view('template/footer'); ?>
 </div>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+<script type="text/javascript" src="<?php echo base_url() . 'JS/jquery.js'; ?>" ></script>
+<script src="<?php echo base_url() . 'JS/popper.js'; ?>" type="text/javascript"></script>
+<script src="<?php echo base_url() . 'JS/bootstrap.js'; ?>" type="text/javascript"></script>
 </body>
 </html>
