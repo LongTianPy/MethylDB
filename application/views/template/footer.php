4864c554988395167496bf9fea9d33b17a28a54e
LongTianPy <tianlongapp@gmail.com>
2018-06-06 09:37:26 -0500

    changed header color

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
-<footer class="footer">
+<footer class="footer fixed-bottom">
     <div class="container">
         <div class="row">
             <div class="col s12">
