cfb106cf389d736ac677248d77aefe606ed482dc
LongTianPy <tianlongapp@gmail.com>
2018-06-06 13:10:44 -0500

    use cover example from bootstrap on welcome page

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -8,14 +8,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="<?php echo base_url() . 'CSS/bootstrap.min.css'; ?>" >
-
+    <link rel="stylesheet" href="<?php echo base_url() . 'CSS/cover.css' ;?>">
     <title>Pan-cancer DNA methylation pattern mining and visualization for biomarker discovery</title>
 </head>
 <body>
-<div class="container">
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
 <?php $this->load->view('template/header'); ?>
-
-
+<main role="main" class="inner hover">
+    <h1 class="cover-heading"> MethylDB</h1>
+    <p class="lead">Pan-cancer DNA methylation pattern mining and visualization for biomarker discovery</p>
+    <p class="lead">
+        <a href="dashboard" class="btn btn-lg btn-secondary">
+            Go to dashboard
+        </a>
+    </p>
+</main>
 <?php $this->load->view('template/footer'); ?>
 </div>
 <script type="text/javascript" src="<?php echo base_url() . 'JS/jquery.js'; ?>" ></script>
