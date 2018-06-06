bfcea86e103c34ccf1940666c1f3c57d342dffe3
LongTianPy <tianlongapp@gmail.com>
2018-06-06 12:36:40 -0500

    added text to navbar

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
     <span class="navbar-brand mb-0 h1">MethylDB</span>
     <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
+        <ul class="navbar-nav mr-auto">
             <li class="nav-item">
                 <a class="nav-link" href="index.php">Home</a>
             </li>
