a2320753420f5c237eaaec98c54c96b9b3f6f320
LongTianPy <tianlongapp@gmail.com>
2018-06-08 17:12:21 -0500

    visualize search id result

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -1,64 +1,48 @@
-/*
-$(function () {
-    $(document).ready(function() {
-        $('#form_search_by_id').submit(function (event) {
-            var cpg_id = $('#cpg_id').val();
-            $.ajax({
-                url: 'index.php/Dashboard',
-                type: 'POST',
-                data:{'cpg_id':cpg_id},
-                success: function(datafile,cpg_id){
-                    document.writeln("something;s going on.");
-                    function makeplot(){
-                        Plotly.d3.csv(datafile,function(data){processData(data)});
-                    };
-                    function processData(allRows){
-                        console.log(allRows);
-                        var acronym_tumor=[], acrynom_normal=[],value_tumor=[],value_normal=[], cpg_id='';
-                        for (var i=0; i<allRows.length;i++){
-                            row=allRows[i];
-                            if (row["TumorNormal"]=='Tumor'){
-                                acronym_tumor.push(row["Acronym"]);
-                                value_tumor.push(row["Value"]);
-                            }
-                            else {
-                                acrynom_normal.push(row["Acronym"]);
-                                value_normal.push(row["Value"]);
-                            }
+function makeplot(){
+    Plotly.d3.csv(datafile,function(data){processData(data)});
+};
+function processData(allRows){
+    console.log(allRows);
+    var acronym_tumor=[], acrynom_normal=[],value_tumor=[],value_normal=[], cpg_id='';
+    for (var i=0; i<allRows.length;i++){
+        row=allRows[i];
+        if (row["TumorNormal"]=='Tumor'){
+            acronym_tumor.push(row["Acronym"]);
+            value_tumor.push(row["Value"]);
+        }
+        else {
+            acrynom_normal.push(row["Acronym"]);
+            value_normal.push(row["Value"]);
+        }
 
-                        }
-                        console.log("Acronym_tumor",acronym_tumor,"Acronym_normal",acrynom_normal,"Value_tumor",value_tumor,"Value_normal",value_normal);
-                        makePlotly(acronym_tumor,value_tumor,acrynom_normal,value_normal);
-                    }
-                    function makePlotly(acronym_tumor,value_tumor,acrynom_normal,value_normal){
-                        var plotDiv = document.getElementById("myChart");
-                        var trace1={
-                            y: value_tumor,
-                            x: acronym_tumor,
-                            name: 'Tumor',
-                            marker: {color: '#FF4136'},
-                            type: 'box'
-                        };
-                        var trace2 = {
-                            y: value_normal,
-                            x: acrynom_normal,
-                            name: "Normal",
-                            marker: {color: '#3D9970'},
-                            type: 'box'
-                        };
-                        var data = [trace1,trace2];
-                        var layout = {
-                            yaxis: {
-                                title: 'DNA Methylation level',
-                                zeroline: false
-                            },
-                            boxmode: 'group'
-                        }
-                        Plotly.newPlot('myChart',data,layout);
-                    }
-                    makeplot();
-                }
-            })
-        })
-    });
-})*/
+    }
+    console.log("Acronym_tumor",acronym_tumor,"Acronym_normal",acrynom_normal,"Value_tumor",value_tumor,"Value_normal",value_normal);
+    makePlotly(acronym_tumor,value_tumor,acrynom_normal,value_normal);
+}
+function makePlotly(acronym_tumor,value_tumor,acrynom_normal,value_normal){
+    var plotDiv = document.getElementById("myChart");
+    var trace1={
+        y: value_tumor,
+        x: acronym_tumor,
+        name: 'Tumor',
+        marker: {color: '#FF4136'},
+        type: 'box'
+    };
+    var trace2 = {
+        y: value_normal,
+        x: acrynom_normal,
+        name: "Normal",
+        marker: {color: '#3D9970'},
+        type: 'box'
+    };
+    var data = [trace1,trace2];
+    var layout = {
+        yaxis: {
+            title: 'DNA Methylation level',
+            zeroline: false
+        },
+        boxmode: 'group'
+    }
+    Plotly.newPlot('myChart',data,layout);
+}
+makeplot();
--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -22,12 +22,11 @@ class Dashboard extends CI_Controller {
     }
 
     public function search_by_id(){
-        session_start();
         $input = "/home/long-lamp-username/MethylDB/mData_output.txt.gz";
         $output = "/home/long-lamp-username/MethylDB/result/result.txt";
         $python_scipt = "/home/long-lamp-username/Mayo_toolbox/prepare_boxplot_data.py";
         $cpg_id = $_POST['cpg_id'];
-        $sql = "select CHR,MAPINFO from Probeset where Probeset_ID={$cpg_id}";
+        $sql = "select CHR,MAPINFO from Probeset where Probeset_ID='{$cpg_id}'";
         $result = $this->db->query($sql)->result(0);
         $chr = $result->CHR;
         $position = $this->MAPINFO;
@@ -40,6 +39,9 @@ class Dashboard extends CI_Controller {
         $datafile = shell_exec($cmd);
         $this->session->set_userdata($datafile);
         $this->session->set_userdata($cpg_id);
+        echo "
+        <script style='display:none;'>var datafile={$datafile},cpg_id={$cpg_id};</script>
+        ";
     }
 
 }
