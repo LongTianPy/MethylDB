65c92c72a09591593b8e7c8c2675f3e950ea30f2
LongTianPy <tianlongapp@gmail.com>
2018-06-11 10:32:25 -0500

    record command executed

--- application/views/dashboardView.php
+++ application/views/dashboardView.php
@@ -117,11 +117,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <script src="/MethylDB/JS/popper.js" type="text/javascript"></script>
 <script src="/MethylDB/JS/bootstrap.js" type="text/javascript"></script>
 <script src="/MethylDB/JS/feather.js" type="text/javascript"></script>
-<script src="/MethylDB/JS/dashboard.js" type="text/javascript"></script>
 <script>
     feather.replace()
 </script>
 <script src="https://cdn.plot.ly/plotly-latest.js"></script>
+<script src="/MethylDB/JS/dashboard.js" type="text/javascript"></script>
 <!--<script src="distrochart.js" charset="utf-8"></script>
 <script src="/MethylDB/JS/chart.js" type="text/javascript"></script>
 <script>
