2cf668bdf9958fbe36e0151ec4ec862a3223061d
LongTianPy <tianlongapp@gmail.com>
2018-06-28 17:05:03 -0500

    uncomment the side navbar

---
+++ application/controllers/Dashboard_dev.php
@@ -0,0 +1,343 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard_dev extends CI_Controller {
+    public function index(){
+        $place_holder = "<img src='/MethylDB/IMG/placeholding_img.png' id='placeholder_img'>";
+        if (isset($_GET['cpg_id'])){
+            $data = $this->search_by_id();
+            if (isset($data['script'])) {
+                $page_data = array(
+                    'place_holder' => $place_holder,
+                    'script' => $data['script'],
+                    'js_parameters'=>$data['js_parameters'],
+                );
+            }else{
+                $page_data = array(
+                    'place_holder' => $place_holder,
+                    'msg' => "No record found, please modify your search.",
+                    );
+            }
+            $this->load->view('dashboardView_dev',$page_data);
+        }
+        elseif (isset($_GET['from']) and isset($_GET['to'])){
+            $data = $this->search_by_region();
+            if (!isset($data['no_result'])){
+                $page_data = array(
+                    'place_holder'=>$place_holder,
+                    'igv'=>1,
+                    'chr'=>$data['chr'],
+                    'start'=>$data['start'],
+                    'end'=>$data['end'],
+                );
+            }else{
+                $page_data = array(
+                    'place_holder' => $place_holder,
+                    'msg' => "No record found, please modify your search.",
+                );
+            }
+            $this->load->view('dashboardView_dev',$page_data);
+        }
+        elseif (isset($_GET['gene'])){
+            $data = $this->search_by_gene();
+            if (!isset($data['no_result'])){
+                $page_data = array(
+                    'place_holder'=>$place_holder,
+                    'igv'=>1,
+                    'chr'=>$data['chr'],
+                    'start'=>$data['start'],
+                    'end'=>$data['end'],
+                );
+            }else{
+                $page_data = array(
+                    'place_holder' => $place_holder,
+                    'msg' => "No record found, please modify your search.",
+                );
+            }
+            $this->load->view('dashboardView_dev',$page_data);
+        }
+    }
+
+    public function search_gene($gene){
+        $sql = "select * from hg19 where geneName='{$gene}' order by exonCount desc limit 1";
+        $result = $this->db->query($sql);
+        $gene_id = $result->transcript_ID;
+        $exonCount = $result->exonCount;
+        $txStart = $result->txStart;
+        $txEnd = $result->txEnd;
+        if ($result->strand == "+"){
+            $strand = 1;
+        }else{
+            $strand = -1;
+        }
+        $exon_item = array();
+        $exonStarts = explode(",",$result->exonStarts);
+        $exonEnds = explode(",",$result->exonEnds);
+        $gene_item = array(
+            'id'=> $gene_id,
+            'start'=> $txStart,
+            'end' => $txEnd,
+            'strand'=>$strand,
+        );
+        $exon_id = 1;
+        for ($i=0;$i<$exonCount;$i++){
+            $exon_item[] = array(
+                'id'=> $gene_id . "_" . $exon_id,
+                'start'=>$exonStarts[$i],
+                'end'=>$exonEnds[$i],
+                'strand'=>$strand,
+            );
+            $exon_id ++ ;
+        }
+        $data = array(
+            "gene_item"=>$gene_item,
+            "exon_items"=>$exon_item,
+        );
+        return $data;
+    }
+
+    public function search_by_id(){
+        $input = "/home/long-lamp-username/MethylDB/mData_output.txt.gz";
+        $output = "/home/long-lamp-username/MethylDB/result/" . uniqid() . ".txt";
+        $python_scipt = "/home/long-lamp-username/Mayo_toolbox/prepare_boxplot_data.py";
+        if (isset($_POST['cpg_id'])){
+            $return_cpg = $this->input->post('cpg_id');
+            $cpg_id = $this->input->post('cpg_id');
+        } else {
+            $return_cpg = $this->input->get('cpg_id');
+            $cpg_id = $this->input->get('cpg_id');
+        }
+        settype($cpg_id,'string');
+        $cpg_id = "'".$cpg_id."'";
+        $sql = "select CHR,MAPINFO from Probeset where Probeset_ID={$cpg_id}";
+        $result = $this->db->query($sql)->result();
+        if (count($result)>0){
+            $result = $result[0];
+            $chr = $result->CHR;
+            $position = $result->MAPINFO;
+            $from = $position-1;
+            $to = $position+1;
+            $cmd = "tabix {$input} {$chr}:{$from}-{$to} -h > {$output}";
+            exec($cmd);
+            $row_nums = count(file($output));
+            settype($row_nums, 'integer');
+            if ($row_nums > 1){
+                $cmd = "python {$python_scipt} {$output}";
+                $datafile = shell_exec($cmd);
+                $this->session->set_userdata($datafile);
+                $this->session->set_userdata($cpg_id);
+                $call_this_script = '<script src="/MethylDB/JS/dashboard.js" type="text/javascript"></script>';
+                $js_parameters = "<script>var datafile='{$datafile}'; var cpg_id='{$return_cpg}'</script>";
+                $data = array('script'=>$call_this_script,
+                    'js_parameters'=>$js_parameters,
+                    );
+            }else{
+                $data = array('no_result' => 0,);
+            }
+        } else{
+            $data = array('no_result' => 0,);
+        }
+        return $data;
+    }
+
+    public function search_by_region(){
+        $input = "/home/long-lamp-username/MethylDB/mData_output.txt.gz";
+        $output = "/home/long-lamp-username/MethylDB/result/" . uniqid() . ".txt";
+        $python_scipt = "/home/long-lamp-username/Mayo_toolbox/prepare_boxplot_multi.py";
+        $chr = $this->input->get('chr_id');
+        $start = $this->input->get('from');
+        $end = $this->input->get('to');
+        $cmd = "tabix {$input} {$chr}:{$start}-{$end} -h > {$output}";
+        exec($cmd);
+        $row_nums = count(file($output));
+        settype($row_nums, 'integer');
+        if ($row_nums > 1){
+            $cmd = "python {$python_scipt} {$output}";
+            $returned = shell_exec($cmd);
+            $returned = explode(",",$returned);
+            $cpg_ids = array_slice($returned,0,-1);
+            $cpg_ids_string = implode(",",$cpg_ids);
+            $datafile = end($returned);
+            $call_this_script = '<script src="/MethylDB/JS/dashboard_gene.js" type="text/javascript"></script>';
+            $sql = "select * from Gene where CHR='{$chr}' and start<={$start} and end>={$end}";
+            $result = $this->db->query($sql)->result();
+            $genes = [];
+            $gene_items = [];
+            $exon_items = [];
+            foreach ($result as $row){
+                $genes[] = $row->gene;
+                $items = $this->search_gene($row->gene);
+                $gene_items[] = $items->gene_item;
+                $exon_items[] = $items->exon_items;
+            }
+            $cpg_items = [];
+            $cpg_iter_id = 1;
+            foreach ($cpg_ids as $cpg_id){
+                $sql = "select * from Probeset where Probeset_ID='{$cpg_id}'";
+                $cpg_result = $this->db->query($sql)->row(0);
+                $locus = $cpg_result->MAPINFO;
+                $cpg_items[] = array(
+                    'id' => $cpg_iter_id,
+                    'bp' => $locus,
+                    'name' => $cpg_id,
+                );
+                $cpg_iter_id ++;
+            }
+            $genes = array(
+                'trackName'=>'track1',
+                'trackType'=>'stranded',
+                'visible'=>true,
+                'inner_radius' =>80,
+                'outer_radius'=>120,
+                'trackFeatures'=>'complex',
+                'featureThreshold'=>7000000,
+                'showLabels'=>true,
+                'showTooltip'=>true,
+                'items'=>$gene_items,
+                'linear_mouseclick'=>'linearPopup',
+            );
+            $exons = array(
+                'trackName'=>'track2',
+                'trackType'=>'stranded',
+                'visible'=>true,
+                'inner_radius' =>195,
+                'outer_radius'=>234,
+                'centre_line_stroke'=>"grey",
+                'showLabels'=>true,
+                'items'=>$exon_items,
+                'linear_mouseclick'=>'linearPopup',
+            );
+            $cpgs = array(
+                'trackName'=>'gapTrack',
+                'trackType'=>'gap',
+                'inner_radius'=>25,
+                'outer_radius'=>235,
+                'showLabels'=>true,
+                'showTooltip'>true,
+                'items'=>$cpg_items,
+            );
+            $tracks = array($genes,$exons,$cpgs);
+            $final_result = array(
+                'chr' => $chr,
+                'from' => $start,
+                'to' => $end,
+                'cpg_ids' => $cpg_ids_string,
+                'script' => $call_this_script,
+                'tracks' => $tracks,
+            );
+        }else{
+            $final_result = array(
+                'no_result' => 0,
+            );
+        }
+        return $final_result;
+    }
+
+    public function search_by_gene(){
+        $input = "/home/long-lamp-username/MethylDB/mData_output.txt.gz";
+        $output = "/home/long-lamp-username/MethylDB/result/" . uniqid() . ".txt";
+        $python_scipt = "/home/long-lamp-username/Mayo_toolbox/prepare_boxplot_multi.py";
+        $gene = $this->input->get('gene');
+        strtoupper($gene);
+        $up = $this->input->get('upstream');
+        $down = $this->input->get('downstream');
+        $sql = "
+        select * from Gene where gene='{$gene}'
+        ";
+        $result = $this->db->query($sql)->result();
+        if (count($result)>0){
+            $chr = $result->CHR;
+            $start = $result->start;
+            $end = $result->end;
+            $new_start = $start - $up;
+            $new_end = $end + $down;
+//            $sql = "select * from Gene where end>={$new_start} and start<={$new_end}";
+//            $result = $this->db->query($sql)->result();
+            $cmd = "tabix {$input} {$chr}:{$new_start}-{$new_end} -h > {$output}";
+            exec($cmd);
+            $row_nums = count(file($output));
+            settype($row_nums, 'integer');
+            if ($row_nums>1){
+                $cmd = "python {$python_scipt} {$output}";
+                $returned = shell_exec($cmd);
+                $returned = explode(",",$returned);
+                $cpg_ids = array_slice($returned,0,-1);
+                $cpg_ids_string = implode(",",$cpg_ids);
+                $datafile = end($returned);
+                $call_this_script = '<script src="/MethylDB/JS/dashboard_gene.js" type="text/javascript"></script>';
+                $items = $this->search_gene($gene);
+                $gene_items = $items['gene_item'];
+                $exon_items = $items['exon_items'];
+                $cpg_items = [];
+                $cpg_iter_id = 1;
+                foreach ($cpg_ids as $cpg_id){
+                    $sql = "select * from Probeset where Probeset_ID='{$cpg_id}'";
+                    $cpg_result = $this->db->query($sql)->row(0);
+                    $locus = $cpg_result->MAPINFO;
+                    $cpg_items[] = array(
+                        'id' => $cpg_iter_id,
+                        'bp' => $locus,
+                        'name' => $cpg_id,
+                    );
+                    $cpg_iter_id ++;
+                }
+                $genes = array(
+                    'trackName'=>'track1',
+                    'trackType'=>'stranded',
+                    'visible'=>true,
+                    'inner_radius' =>80,
+                    'outer_radius'=>120,
+                    'trackFeatures'=>'complex',
+                    'featureThreshold'=>7000000,
+                    'showLabels'=>true,
+                    'showTooltip'=>true,
+                    'items'=>$gene_items,
+                    'linear_mouseclick'=>'linearPopup',
+                );
+                $exons = array(
+                    'trackName'=>'track2',
+                    'trackType'=>'stranded',
+                    'visible'=>true,
+                    'inner_radius' =>195,
+                    'outer_radius'=>234,
+                    'centre_line_stroke'=>"grey",
+                    'showLabels'=>true,
+                    'items'=>$exon_items,
+                    'linear_mouseclick'=>'linearPopup',
+                );
+                $cpgs = array(
+                    'trackName'=>'gapTrack',
+                    'trackType'=>'gap',
+                    'inner_radius'=>25,
+                    'outer_radius'=>235,
+                    'showLabels'=>true,
+                    'showTooltip'>true,
+                    'items'=>$cpg_items,
+                );
+                $tracks = array($genes,$exons,$cpgs);
+                $data = array(
+                    'chr' => $chr,
+                    'from' => $start,
+                    'to' => $end,
+                    'cpg_ids' => $cpg_ids_string,
+                    'script' => $call_this_script,
+                    'tracks' => $tracks,
+                );
+            }
+            else {
+                $data = array('no_result'=>0);
+            }
+        }
+        else {
+            $data = array('no_result'=>0);
+        }
+        return $data;
+    }
+
+
+}
+
+
+
+?>
+<!DOCTYPE html>
---
+++ application/views/dashboardView_dev.php
@@ -0,0 +1,183 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+    <link rel="stylesheet" href="/MethylDB/CSS/bootstrap.min.css" >
+    <link rel="stylesheet" href="/MethylDB/CSS/dashboard.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/10.0.2/css/bootstrap-slider.css">
+    <link rel="stylesheet" type="text/css" href="https://igv.org/web/release/1.0.9/igv-1.0.9.css">
+    <title>Pan-cancer DNA methylation pattern mining and visualization for biomarker discovery</title>
+</head>
+<body>
+<nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="<?php echo base_url(); ?>">MethylDB</a>
+    <span class="w-100 navbar-text">Pan-cancer DNA methylation pattern mining and visualization for biomarker discovery</span>
+    <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+            <a class="nav-link" href="index.php/About">About</a>
+        </li>
+    </ul>
+</nav>
+<div class="container-fluid">
+    <div class="row">
+        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+            <div class="sidebar-sticky">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <h5>
+                            <a class="nav-link active" href="#">
+                                <span data-feather="home"></span>
+                                Dashboard <span class="sr-only">(current)</span>
+                            </a>
+                        </h5>
+                    </li>
+                    <li class="nav-item" id="search_functions">
+                        <h5>
+                            <a class="nav-link" href="#">
+                                <span data-feather="file"></span>
+                                Search
+                            </a>
+                        </h5>
+                        <div class="card">
+                            <div class="card-header" id="search_by_id">
+                                <h6 class="mb-0">
+                                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#form_by_id" aria-expanded="false" aria-controls="form_by_id">
+                                        Search by CpG ID
+                                    </button>
+                                </h6>
+                            </div>
+                            <div id="form_by_id" class="collapse show" aria-labelledby="search_by_id" data-parent="#search_functions" >
+                                <div class="card-body">
+                                    <form method="get" id="form_search_by_id" action="Dashboard">
+                                        <div class="form-group">
+                                            <label for="cpg_id">CpG probe ID</label>
+                                            <input type="text" class="form-control" name="cpg_id" id="cpg_id" placeholder="e.g. cg00000029" required>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary">Go</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="search_by_gene">
+                                <h6 class="mb-0">
+                                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#form_by_gene" aria-expanded="false" aria-controls="form_by_gene">
+                                        Search by gene name
+                                    </button>
+                                </h6>
+                            </div>
+                            <div id="form_by_gene" class="collapse" aria-labelledby="search_by_gene" data-parent="#search_functions">
+                                <div class="card-body">
+                                    <form method="get" id="form_search_by_gene" action="Dashboard">
+                                        <div class="form-group">
+                                            <label for="gene">Gene name</label>
+                                            <input type="text" class="form-control" name="gene" id="gene" placeholder="e.g. EGFR" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="upstream">Upstream</label>
+                                            <input type="text" class="form-control" name="upstream" id="upstream" placeholder="Default 0, optional" />bp
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="downstream">Downstream</label>
+                                            <input type="text" class="form-control" name="downstream" id="downstream" placeholder="Default 0, optional" />bp
+                                        </div>
+                                        <button type="submit" class="btn btn-primary">Go</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="search_by_region">
+                                <h6 class="mb-0">
+                                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#form_by_region" aria-expanded="false" aria-controls="form_by_region">
+                                        Search by genomic region
+                                    </button>
+                                </h6>
+                            </div>
+                            <div id="form_by_region" class="collapse" aria-labelledby="search_by_region" data-parent="#search_functions">
+                                <div class="card-body">
+                                    <form method="get" id="form-search_by_region" action="Dashboard">
+                                        <div class="form-group">
+                                            <label for="chr_id">Chromosome</label>
+                                            <select class="form-control" id="chr_id" name="chr_id">
+                                                <?php
+                                                for ($i=1;$i<=22;$i++) {
+                                                    echo "<option>{$i}</option>";
+                                                }
+                                                ?>
+                                                <option>X</option>
+                                                <option>Y</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="from">From</label>
+                                            <input type="text" id="from" class="form-control" name="from" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="to">To</label>
+                                            <input type="text" id="to" class="form-control" name="to" required>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary">Go</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+            <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 class="h2">Dashboard</h1>
+                <div class="btn-toolbar mb-2 mb-md-0">
+                    <div class="btn-group mr-2">
+                        <button class="btn btn-sm btn-outline-secondary">Share</button>
+                        <button class="btn btn-sm btn-outline-secondary">Export</button>
+                    </div>
+                </div>
+            </div>
+            <?php
+            if (isset($igv)){
+                echo "<div id='igvDiv' style='padding-top:10px;padding-bottom: 10px;border: 1px solid lightgrey'></div>";
+            }
+            ?>
+            <div class="my-4 w-100 figure-img img-fluid rounded" id="myChart" width="900" height="600" style="min-height: 600px;">
+                <?php
+                if (isset($place_holder)) {
+                    echo $place_holder;
+                }
+                ?>
+            </div>
+            <h2>Methylation beta values</h2>
+            <table class="table table-striped table-sm">
+
+            </table>
+        </main>
+    </div>
+</div>
+<script src="/MethylDB/JS/jquery.js" type="text/javascript"></script>
+<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.js" type="text/javascript"></script>
+<script src="/MethylDB/JS/popper.js" type="text/javascript"></script>
+<script src="/MethylDB/JS/bootstrap.js" type="text/javascript"></script>
+<script src="/MethylDB/JS/feather.js" type="text/javascript"></script>
+<script>
+    feather.replace()
+</script>
+<script type="text/javascript" src="https://igv.org/web/release/1.0.9/igv-1.0.9.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-slider/10.0.2/bootstrap-slider.js" type="text/javascript"></script>
+<script src="https://cdn.plot.ly/plotly-latest.js"></script>
+<?php
+if (isset($igv)){
+    echo "<script>var chr={$chr}; var start={$start}; var end={$end}</script>";
+    echo "<script src='/MethylDB/JS/run_igv.js' type='text/javascript'></script>";
+}
+?>
+</body>
+</html>
