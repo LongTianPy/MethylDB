35cdead7ef71ff46c00b22018f7ca95f949927f6
LongTianPy <tianlongapp@gmail.com>
2018-06-29 10:18:07 -0500

    enable search by id in dashboard_dev

--- JS/run_igv.js
+++ JS/run_igv.js
@@ -21,10 +21,8 @@ var options = {
 
         {
             name: "DNA Methylation CpG sites",
-            format: "vcf",
             url: "/MethylDB/Data/sorted_cpg.txt.gz",
             indexURL:  "/MethylDB/Data/sorted_cpg.txt.gz.tbi",
-            type: "variant"
         }
     ]
 
