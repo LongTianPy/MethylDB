a7d857be6ca2bf60190971cd15fa76eceace17f7
LongTianPy <tianlongapp@gmail.com>
2018-07-02 11:24:27 -0500

    enable search by id in dashboard_dev

--- JS/run_igv.js
+++ JS/run_igv.js
@@ -1,31 +1,66 @@
-var div = document.getElementById('igvDiv');
-var options = {
-    reference: {
-        id: "hg19",
-        fastaURL: "https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/1kg_v37/human_g1k_v37_decoy.fasta",
-        cytobandURL: "https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/b37/b37_cytoband.txt"
-    },
-    locus: chromosome.toString() + ":" + start.toString() + "-" + end.toString(),
-    tracks: [
-        {
-            name: "Genes",
-            type: "annotation",
-            format: "bed",
-            sourceType: "file",
-            url: "https://s3.amazonaws.com/igv.broadinstitute.org/annotations/hg19/genes/refGene.hg19.bed.gz",
-            indexURL: "https://s3.amazonaws.com/igv.broadinstitute.org/annotations/hg19/genes/refGene.hg19.bed.gz.tbi",
-            order: Number.MAX_VALUE,
-            visibilityWindow: 300000000,
-            displayMode: "EXPANDED"
+// var div = document.getElementById('igvDiv');
+// var options = {
+//     reference: {
+//         id: "hg19",
+//         fastaURL: "https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/1kg_v37/human_g1k_v37_decoy.fasta",
+//         cytobandURL: "https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/b37/b37_cytoband.txt"
+//     },
+//     locus: chromosome.toString() + ":" + start.toString() + "-" + end.toString(),
+//     tracks: [
+//         {
+//             name: "Genes",
+//             type: "annotation",
+//             format: "bed",
+//             sourceType: "file",
+//             url: "https://s3.amazonaws.com/igv.broadinstitute.org/annotations/hg19/genes/refGene.hg19.bed.gz",
+//             indexURL: "https://s3.amazonaws.com/igv.broadinstitute.org/annotations/hg19/genes/refGene.hg19.bed.gz.tbi",
+//             order: Number.MAX_VALUE,
+//             visibilityWindow: 300000000,
+//             displayMode: "EXPANDED"
+//         },
+//
+//         {
+//             name: "DNA Methylation CpG sites",
+//             format: "bed",
+//             url: "/MethylDB/Data/modified_cpg.txt.gz",
+//             indexURL:  "/MethylDB/Data/modified_cpg.txt.gz.tbi",
+//         }
+//     ]
+//
+// };
+// var browser = igv.createBrowser(div, options);
+
+$(document).ready(function () {
+    var div = document.getElementById('igvDiv');
+    var options = {
+        reference: {
+            id: "hg19",
+            fastaURL: "https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/1kg_v37/human_g1k_v37_decoy.fasta",
+            cytobandURL: "https://s3.amazonaws.com/igv.broadinstitute.org/genomes/seq/b37/b37_cytoband.txt"
         },
+        locus: chromosome.toString() + ":" + start.toString() + "-" + end.toString(),
+        tracks: [
+            {
+                name: "Genes",
+                type: "annotation",
+                format: "bed",
+                sourceType: "file",
+                url: "https://s3.amazonaws.com/igv.broadinstitute.org/annotations/hg19/genes/refGene.hg19.bed.gz",
+                indexURL: "https://s3.amazonaws.com/igv.broadinstitute.org/annotations/hg19/genes/refGene.hg19.bed.gz.tbi",
+                order: Number.MAX_VALUE,
+                visibilityWindow: 300000000,
+                displayMode: "EXPANDED"
+            },
 
-        {
-            name: "DNA Methylation CpG sites",
-            format: "bed",
-            url: "/MethylDB/Data/modified_cpg.txt.gz",
-            indexURL:  "/MethylDB/Data/modified_cpg.txt.gz.tbi",
-        }
-    ]
+            {
+                name: "DNA Methylation CpG sites",
+                format: "bed",
+                url: "/MethylDB/Data/modified_cpg.txt.gz",
+                indexURL:  "/MethylDB/Data/modified_cpg.txt.gz.tbi",
+            }
+        ]
 
-};
-var browser = igv.createBrowser(div, options);
+    };
+    var browser = igv.createBrowser(div, options);
+
+});
