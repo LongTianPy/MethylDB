5209d5aa248948d508616c3ac4bad7f6b93ed949
LongTianPy <tianlongapp@gmail.com>
2018-07-02 11:32:40 -0500

    enable search by id in dashboard_dev

--- JS/run_igv.js
+++ JS/run_igv.js
@@ -31,6 +31,57 @@
 // var browser = igv.createBrowser(div, options);
 
 $(document).ready(function () {
+    var processData = function(allRows,cpg_id){
+        console.log(allRows);
+        var acronym_tumor=[], acronym_normal=[],value_tumor=[],value_normal=[];
+        for (var i=0; i<allRows.length;i++){
+            row=allRows[i];
+            if (row["TumorNormal"]=='Tumor'){
+                acronym_tumor.push(row["Acronym"]);
+                value_tumor.push(row["Value"]);
+            }
+            else {
+                acronym_normal.push(row["Acronym"]);
+                value_normal.push(row["Value"]);
+            }
+
+        }
+        console.log("Acronym_tumor",acronym_tumor,"Acronym_normal",acronym_normal,"Value_tumor",value_tumor,"Value_normal",value_normal,'cpg_id',cpg_id);
+        makePlotly(acronym_tumor,value_tumor,acronym_normal,value_normal,cpg_id);
+    }
+    var makePlotly = function(acronym_tumor,value_tumor,acronym_normal,value_normal,cpg_id){
+        var plotDiv = document.getElementById("myChart");
+        var trace1={
+            y: value_tumor,
+            x: acronym_tumor,
+            name: 'Tumor',
+            marker: {color: '#FF4136'},
+            type: 'box'
+        };
+        var trace2 = {
+            y: value_normal,
+            x: acronym_normal,
+            name: "Normal",
+            marker: {color: '#3D9970'},
+            type: 'box'
+        };
+        var data = [trace1,trace2];
+        var layout = {
+            title: cpg_id,
+            yaxis: {
+                title: 'DNA Methylation level',
+                zeroline: false
+            },
+            xaxis: {
+                tickangle: 270
+            },
+            boxmode: 'group'
+        }
+        document.getElementById("placeholder_img").style.display = "none";
+        Plotly.react('myChart',data,layout);
+    }
+
+
     var div = document.getElementById('igvDiv');
     var options = {
         reference: {
@@ -62,5 +113,15 @@ $(document).ready(function () {
 
     };
     var browser = igv.createBrowser(div, options);
-
+    browser.on('trackclick',function (track,popoverData) {
+        if (track.name=="DNA Methylation CpG sites") {
+            popoverData.forEach(function (nameValue) {
+                if (nameValue.name == "Name"){
+                    var cpg_id = nameValue.value;
+                    var file = "/MethylDB/Result/" + cpg_id + ".txt";
+                    Plotly.d3.csv(file,function(data){processData(data,cpg_id)});
+                }
+            })
+        }
+    })
 });
