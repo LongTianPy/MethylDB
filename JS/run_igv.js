d6d7bdc00aa5e0a4c0f37624127b0d709c18b823
LongTianPy <tianlongapp@gmail.com>
2018-07-16 15:05:16 -0500

    relocate the website to a bigger disk

--- JS/run_igv.js
+++ JS/run_igv.js
@@ -121,6 +121,7 @@ $(document).ready(function () {
 
     var browser = igv.createBrowser(div, options);
     browser.on('trackclick',function (track,popoverData) {
+
         if (track.name=="DNA Methylation CpG sites") {
             popoverData.forEach(function (nameValue) {
                 if (nameValue.name == "Name"){
@@ -132,7 +133,8 @@ $(document).ready(function () {
                     if (FileExists(file)){
                         Plotly.d3.csv(file,function(data){processData(data,cpg_id)});
                     } else {
-                        $("#myChart").append("<div class='d-flex justify-content-center'><h2>No Data Found For This CpG Site</h2></div>");
+                        $("#placeholder_img").hide();
+                        $("#myChart").html("<div class='d-flex justify-content-center'><h2>No Data Found For This CpG Site</h2></div>");
                     }
 
                 }
