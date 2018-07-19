c10c933170116c940004dd72f1de08b2e7ca73bd
LongTianPy <tianlongapp@gmail.com>
2018-07-19 16:30:14 -0500

    add t test to single cpg id search

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -104,9 +104,9 @@ $(document).ready(function(){
         }
         document.getElementById("placeholder_img").style.display = "none";
         Plotly.react('myChart', data, layout);
-        var tscore=ss.tTestTwoSample(value_normal,value_tumor,0);
+        var tscore=jStat.tscore(value_normal,value_tumor);
         console.log(tscore);
-        var pvalue = jStat.ttest(1.6,value_tumor.length+value_normal.length-2,2);
+        var pvalue = jStat.ttest(tscore,value_tumor.length+value_normal.length-2,2);
         console.log(pvalue);
         $('#stats_output').html('<table><tr><td>T-test between normal and tumor samples</td><td>' + pvalue +'</td></tr></table>');
     }
